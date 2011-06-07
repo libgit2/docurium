90e2f30008309929e33e822a043084f90a75f57b
Scott Chacon <schacon@gmail.com>
2011-06-07 11:14:31 -0700

    got it listing files

--- docs/js/docurium.js
+++ docs/js/docurium.js
@@ -2,8 +2,9 @@ $(function() {
   // our document model - stores the datastructure generated from docurium
   var Docurium = Backbone.Model.extend({
 
+    defaults: {'version': 'unknown'},
+
     initialize: function() {
-      this.set({'version': 'unknown'})
       this.loadVersions()
     },
 
@@ -16,17 +17,73 @@ $(function() {
 
     loadDoc: function() {
       version = this.get('version')
-      $.get(version + '.json', function(data) {
-        docurium.set({'files': data})
+      $.ajax({
+        url: version + '.json',
+        context: this,
+        success: function(data){
+          console.log(this.get('version'))
+          fs = _.map(data, function(val, key) {
+            return new DocFile({file: key, functions: val['functions'], meta: val['meta']})
+          })
+          files.refresh(fs)
+        }
       })
     },
-
   })
 
-  window.docurium = new Docurium
+  var DocFile = Backbone.Model.extend({
+  })
 
+  var DocFileGroup = Backbone.Collection.extend({
+    model: DocFile
+  })
+
+
+  var DocFileView = Backbone.View.extend({
+    tagName: "li",
+    className: "file-entry",
+
+    initialize: function() {
+      _.bindAll(this, "render")
+    },
+
+    render: function() {
+      $(this.el).html("<h3><a>" + this.model.get('file') + "</a></h3>"); 
+      return this
+    }
+  })
+
+  var DocFileGroupView = Backbone.View.extend({
+    tagName: "li",
+    className: "file-group",
+
+    initialize: function() {
+      _.bindAll(this, "render")
+      this.collection.bind('all',     this.render);
+    },
+
+    render: function() {
+      $("#files-list").empty()
+      _.each(this.collection.models, function(doc) {
+        console.log(doc)
+        var view = new DocFileView({model: doc})
+        $("#files-list").append(view.render().el)
+      })
+      return this
+    }
+  })
+
+  var DocuriumView = Backbone.View.extend({
+  })
+    
+  window.docurium = new Docurium
+  window.files = new DocFileGroup
+
+  // gonna wanna do this in docuriumview, i think
   docurium.bind('change:version', function(model, version) {
     $('#version').text(version)
   })
+
+  window.App = new DocFileGroupView({collection: files})
 
 })
