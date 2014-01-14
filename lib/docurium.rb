3e17133b02268e8e795dd70604bde1caf312b489
Carlos Martín Nieto <cmn@dwim.me>
2014-01-14 16:15:18 +0100

    Factor out example compilation

--- lib/docurium.rb
+++ lib/docurium.rb
@@ -44,6 +44,57 @@ class Docurium
     opt
   end
 
+  def compile_examples(index, tf, version)
+    files = []
+    index.each do |entry|
+      next unless entry[:path].match(/\.c$/)
+      files << entry[:path]
+    end
+
+    output = []
+    files.each do |file|
+      out "    # #{file}"
+
+      # highlight, roccoize and link
+      rocco = Rocco.new(file, files, {:language => 'c'}) do
+        ientry = index[file]
+        blob = @repo.lookup(ientry[:oid])
+        blob.content
+      end
+      rocco_layout = Rocco::Layout.new(rocco, tf)
+      rocco_layout.version = version
+      rf = rocco_layout.render
+
+      extlen = -(File.extname(file).length + 1)
+      rf_path = file[0..extlen] + '.html'
+      rel_path = "ex/#{version}/#{rf_path}"
+
+      # look for function names in the examples and link
+      id_num = 0
+      @data[:functions].each do |f, fdata|
+        rf.gsub!(/#{f}([^\w])/) do |fmatch|
+          extra = $1
+          id_num += 1
+          name = f + '-' + id_num.to_s
+          # save data for cross-link
+          @data[:functions][f][:examples] ||= {}
+          @data[:functions][f][:examples][file] ||= []
+          @data[:functions][f][:examples][file] << rel_path + '#' + name
+          "<a name=\"#{name}\" class=\"fnlink\" href=\"../../##{version}/group/#{fdata[:group]}/#{f}\">#{f}</a>#{extra}"
+        end
+      end
+
+      # write example to the repo
+      sha = @repo.write(rf, :blob)
+      output << {:path => rel_path, :oid => sha, :mode => 0100644}
+
+      @data[:examples] ||= []
+      @data[:examples] << [file, rel_path]
+    end
+
+    output
+  end
+
   def generate_docs
     out "* generating docs"
     output_index = Rugged::Index.new
@@ -64,51 +115,9 @@ class Docurium
         if subtree = find_subtree(version, ex) # check that it exists
           index.read_tree(subtree)
           out "  - processing examples for #{version}"
-
-          files = []
-          index.each do |entry|
-            next unless entry[:path].match(/\.c$/)
-            files << entry[:path]
-          end
-
-          files.each do |file|
-            out "    # #{file}"
-
-            # highlight, roccoize and link
-            rocco = Rocco.new(file, files, {:language => 'c'}) do
-              ientry = index[file]
-              blob = @repo.lookup(ientry[:oid])
-              blob.content
-            end
-            rocco_layout = Rocco::Layout.new(rocco, tf)
-            rocco_layout.version = version
-            rf = rocco_layout.render
-
-            extlen = -(File.extname(file).length + 1)
-            rf_path = file[0..extlen] + '.html'
-            rel_path = "ex/#{version}/#{rf_path}"
-
-            # look for function names in the examples and link
-            id_num = 0
-            @data[:functions].each do |f, fdata|
-              rf.gsub!(/#{f}([^\w])/) do |fmatch|
-                extra = $1
-                id_num += 1
-                name = f + '-' + id_num.to_s
-                # save data for cross-link
-                @data[:functions][f][:examples] ||= {}
-                @data[:functions][f][:examples][file] ||= []
-                @data[:functions][f][:examples][file] << rel_path + '#' + name
-                "<a name=\"#{name}\" class=\"fnlink\" href=\"../../##{version}/group/#{fdata[:group]}/#{f}\">#{f}</a>#{extra}"
-              end
-            end
-
-            # write example to the repo
-            sha = @repo.write(rf, :blob)
-            output_index.add(:path => rel_path, :oid => sha, :mode => 0100644)
-
-            @data[:examples] ||= []
-            @data[:examples] << [file, rel_path]
+          output = compile_examples(index, tf, version)
+          output.each do |f|
+            output_index.add(f)
           end
         end
 
