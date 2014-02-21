57cde7d08986f2730aa8106df043e07d70eed3bd
Carlos Martín Nieto <cmn@dwim.me>
2014-02-21 15:44:44 +0100

    Capture gen's output

--- test/gen_test.rb
+++ test/gen_test.rb
@@ -8,9 +8,11 @@ class GenTest < MiniTest::Unit::TestCase
   # make sure we can read what we give the user
   def test_read_generated_file
     file = Tempfile.new 'docurium'
-    Docurium::CLI.gen(file.path)
+    capture_io do
+      Docurium::CLI.gen(file.path)
+    end
 
-    Docurium.new file.path
+      Docurium.new file.path
   end
 
 end
