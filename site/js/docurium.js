547279f752144d1ac968df44c0566cd3220507d6
Etienne Samson <samson.etienne@gmail.com>
2019-09-15 08:57:41 +0200

    js: remove for loop, rescope local variables

--- site/js/docurium.js
+++ site/js/docurium.js
@@ -600,18 +600,17 @@ $(function() {
       types = this.get('data')['types']
       var version = this.get('version')
 
-      for(var i=0; i<types.length; i++) {
-        type = types[i]
-        typeName = type[0]
-        typeData = type[1]
-        re = new RegExp(typeName + '\\s', 'gi');
+      _.each(types, function(type) {
+        var typeName = type[0];
+        var typeData = type[1];
+        var re = new RegExp(typeName + '\\s', 'gi');
         var link = $('<a>').attr('href', '#' + typeLink(typeName, version)).append(typeName)[0]
         text = text.replace(re, link.outerHTML + ' ')
-      }
+      });
 
       var callbacks = this.get('data')['callbacks']
       _.each(callbacks, function(cb, typeName) {
-        re = new RegExp(typeName + '$', 'gi');
+        var re = new RegExp(typeName + '$', 'gi');
         var link = $('<a>').attr('href', '#' + functionLink('callback', typeName, version)).append(typeName)[0]
         text = text.replace(re, link.outerHTML + ' ')
       });
